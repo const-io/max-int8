be507cfca4f0a063272e6c05308b307b112a3e90
kgryte <kgryte@gmail.com>
2016-03-29 04:34:14 -0700

    Update dotfiles, deps, and repo url

--- examples/index.js
+++ examples/index.js
@@ -3,3 +3,4 @@
 var MAX_INT8 = require( './../lib' );
 
 console.log( MAX_INT8 );
+// returns 127
--- test/test.js
+++ test/test.js
@@ -1,27 +1,21 @@
-/* global require, describe, it */
 'use strict';
 
 // MODULES //
 
-var // Expectation library:
-	chai = require( 'chai' ),
-
-	// Module to be tested:
-	CONST = require( './../lib' );
-
-
-// VARIABLES //
-
-var expect = chai.expect,
-	assert = chai.assert;
+var tape = require( 'tape' );
+var pow = require( 'math-power' );
+var MAX_INT8 = require( './../lib' );
 
 
 // TESTS //
 
-describe( 'compute-const-max-int8', function tests() {
+tape( 'the main export is a number', function test( t ) {
+	t.ok( true, __filename );
+	t.equal( typeof MAX_INT8, 'number', 'main export is a number' );
+	t.end();
+});
 
-	it( 'should export a number', function test() {
-		expect( CONST ).to.be.a( 'number' );
-	});
-
+tape( 'the exported value equals 2**7 - 1', function test( t ) {
+	t.equal( MAX_INT8, pow(2,7)-1, 'equals 2**7-1' );
+	t.end();
 });
